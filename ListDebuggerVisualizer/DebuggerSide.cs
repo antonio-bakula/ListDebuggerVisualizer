76ee0df59bc5c1d0560e017e912a8577dcc46731
Antonio Bakula <antonio@wem.hr>
2019-10-01 01:02:23 +0200

    Visualizer now can inspect lists that are note marked with Serializable attribute

--- ListDebuggerVisualizer/DebuggerSide.cs
+++ ListDebuggerVisualizer/DebuggerSide.cs
@@ -3,74 +3,104 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 
-[assembly: DebuggerVisualizer(typeof(ListDebuggerVisualizer.ListDebuggerVisualizerClient), typeof(VisualizerObjectSource), Target = typeof(List<>),
-           Description = "List Debugger Visualizer")]
+[assembly: DebuggerVisualizer(typeof(ListDebuggerVisualizer.ListDebuggerVisualizerClient), typeof(ListDebuggerVisualizer.VisualizerJsonObjectSource), Target = typeof(List<>),
+					 Description = "List Debugger Visualizer")]
 
 namespace ListDebuggerVisualizer
 {
-  public class ListDebuggerVisualizerClient : DialogDebuggerVisualizer
-  {
-    override protected void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
-    {
-      try
-      {
-        ShowVisualizer(objectProvider);
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show("Exception getting object data: " + ex.Message);
-      }
-    }
+	public class ListDebuggerVisualizerClient : DialogDebuggerVisualizer
+	{
+		override protected void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+		{
+			try
+			{
+				ShowVisualizer(objectProvider);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Exception getting object data: " + ex.Message);
+			}
+		}
 
-    private static void ShowVisualizer(IVisualizerObjectProvider objectProvider)
-    {
-      var data = (IList)objectProvider.GetObject();
-      var itemType = data.GetType().GetProperty("Item").PropertyType;
-      if (itemType == typeof(string))
-      {
-        var prim = new List<PrimitiveListItem>();
-        foreach (string strItem in data.Cast<string>())
-        {
-          var pli = new PrimitiveListItem();
-          pli.Value = strItem;
-          prim.Add(pli);
-        }
-        ListDebuggerVisualizerClient.ShowVisualizerForm(prim, itemType.Name);
-      }
-      else if (itemType.IsPrimitive)
-      {
-        var prim = new List<PrimitiveListItem>();
-        foreach (object objItem in data.Cast<object>())
-        {
-          var pli = new PrimitiveListItem();
-          pli.Value = objItem.ToString();
-          prim.Add(pli);
-        }
-        ListDebuggerVisualizerClient.ShowVisualizerForm(prim, itemType.Name);
-      }
-      else
-      {
-        ListDebuggerVisualizerClient.ShowVisualizerForm(data, itemType.Name);
-      }
-    }
+		private static void ShowVisualizer(IVisualizerObjectProvider objectProvider)
+		{
+			CosturaUtility.Initialize();
 
-    public static void ShowVisualizerForm(IList data, string typeName)
-    {
-      var mf = new MainForm();
-      mf.Model = data;
-      mf.ListType = typeName;
-      mf.ShowDialog();
-    }
-  }
+			var jsonStream = objectProvider.GetData();
+			var reader = new StreamReader(jsonStream);
+			string json = reader.ReadToEnd();
+			var container = Newtonsoft.Json.JsonConvert.DeserializeObject<VisualizerDataContainer>(json);
 
+			if (container.TypeName == "string")
+			{
+				var prim = new List<PrimitiveListItem>();
+				foreach (string strItem in container.Data.Cast<string>())
+				{
+					var pli = new PrimitiveListItem();
+					pli.Value = strItem;
+					prim.Add(pli);
+				}
+				ListDebuggerVisualizerClient.ShowVisualizerForm(prim, container.TypeName);
+			}
+			else if (container.IsPrimitive)
+			{
+				var prim = new List<PrimitiveListItem>();
+				foreach (object objItem in container.Data.Cast<object>())
+				{
+					var pli = new PrimitiveListItem();
+					pli.Value = objItem?.ToString() ?? "";
+					prim.Add(pli);
+				}
+				ListDebuggerVisualizerClient.ShowVisualizerForm(prim, container.TypeName);
+			}
+			else
+			{
+				ListDebuggerVisualizerClient.ShowVisualizerForm(container.Data, container.TypeName);
+			}
+		}
 
-  public class PrimitiveListItem
-  {
-    public string Value { get; set; }
-  }
+		public static void ShowVisualizerForm(IList data, string typeName)
+		{
+			var mf = new MainForm();
+			mf.Model = data;
+			mf.ListType = typeName;
+			mf.ShowDialog();
+		}
+	}
+
+	public class PrimitiveListItem
+	{
+		public string Value { get; set; }
+	}
+
+	public class VisualizerJsonObjectSource : VisualizerObjectSource
+	{
+		public override void GetData(object target, Stream outgoingData)
+		{
+			var itemType = target.GetType().GetProperty("Item").PropertyType;
+			var container = new VisualizerDataContainer();
+			container.TypeName =itemType.Name;
+			container.Data = (IList)target;
+			container.IsPrimitive = itemType.IsPrimitive;
+
+			string json = Newtonsoft.Json.JsonConvert.SerializeObject(container);
+			var writer = new StreamWriter(outgoingData);
+			writer.WriteLine(json);
+			writer.Flush();
+		}
+	}
+
+	public class VisualizerDataContainer
+	{
+		public string TypeName { get; set; }
+		public IList Data { get; set; }
+		public bool IsPrimitive { get; set; }
+	}
+
 }
--- ListDebuggerVisualizer/MainForm.cs
+++ ListDebuggerVisualizer/MainForm.cs
@@ -16,206 +16,218 @@ using Telerik.WinControls.UI;
 
 namespace ListDebuggerVisualizer
 {
-  public partial class MainForm : Form
-  {
-    public IList Model { get; set; }
-    public string ListType { get; set; }
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// if exceptions occures when Visualizer form is visible (and modal) Visual Studio chrashes when closing form, and user cannot close Visualizer form. It's annoying, very.
+		/// </summary>
+		private bool formLoaded = false;
+		public IList Model { get; set; }
+		public string ListType { get; set; }
 
-    private RadGridView grid;
+		private RadGridView grid;
 
-    public MainForm()
-    {
-      InitializeComponent();
-    }
+		public MainForm()
+		{
+			InitializeComponent();
+		}
 
-    private void MainForm_Load(object sender, EventArgs e)
-    {
-      if (this.Model == null)
-        return;
-      InitGrid();
-      this.grid.DataSource = this.Model;
-      AutoFitColumns();
-      ReadSettings();
-      toolStripLabelTypeName.Text = "List item type: " + this.ListType;
-    }
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			if (this.Model == null)
+			{
+				return;
+			}
+			InitGrid();
+			this.grid.DataSource = this.Model;
+			AutoFitColumns();
+			ReadSettings();
+			toolStripLabelTypeName.Text = "List item type: " + this.ListType;
+			this.formLoaded = true;
+		}
 
-    private void InitGrid()
-    {
-      ThemeResolutionService.ApplicationThemeName = "Office2010Blue";
+		private void InitGrid()
+		{
+			ThemeResolutionService.ApplicationThemeName = "Office2010Blue";
 
-      this.grid = new RadGridView();
-      panelContent.Controls.Add(this.grid);
+			this.grid = new RadGridView();
+			panelContent.Controls.Add(this.grid);
 
-      this.grid.ReadOnly = true;
-      this.grid.Dock = DockStyle.Fill;
-      this.grid.MasterTemplate.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.None;
+			this.grid.ReadOnly = true;
+			this.grid.Dock = DockStyle.Fill;
+			this.grid.MasterTemplate.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.None;
 
-      this.grid.AllowSearchRow = true;
+			this.grid.AllowSearchRow = true;
 
-      this.grid.EnableFiltering = true;
-      this.grid.MasterTemplate.ShowHeaderCellButtons = true;
-      this.grid.MasterTemplate.ShowFilteringRow = false;
-    }
+			this.grid.EnableFiltering = true;
+			this.grid.MasterTemplate.ShowHeaderCellButtons = true;
+			this.grid.MasterTemplate.ShowFilteringRow = false;
 
-    private void AutoFitColumns()
-    {
-      this.grid.MasterTemplate.BestFitColumns(BestFitColumnMode.AllCells);
-    }
+			this.grid.AutoGenerateHierarchy = true;
+		}
 
-    private void ReadSettings()
-    {
-      var settings = GetSettingsList();
-      if (settings == null)
-        return;
+		private void AutoFitColumns()
+		{
+			this.grid.MasterTemplate.BestFitColumns(BestFitColumnMode.AllCells);
+		}
 
-      var mySetting = settings.FirstOrDefault(ltis => ltis.Name == this.ListType);
-      if (mySetting != null)
-      {
-        this.Location = mySetting.Location;
-        this.Size = mySetting.Size;
-        this.grid.LoadLayout(mySetting.GridSettingsFile);
-      }
-    }
+		private void ReadSettings()
+		{
+			var settings = GetSettingsList();
+			if (settings == null)
+				return;
 
-    private void SaveSettings()
-    {
-      ListTypeItemSettings mySetting = null;
-      var settingsFile = new FileInfo(GetSettingsStorageFile());
+			var mySetting = settings.FirstOrDefault(ltis => ltis.Name == this.ListType);
+			if (mySetting != null)
+			{
+				this.Location = mySetting.Location;
+				this.Size = mySetting.Size;
+				this.grid.LoadLayout(mySetting.GridSettingsFile);
+			}
+		}
 
-      var settings = GetSettingsList();
-      if (settings != null)
-      {
-        mySetting = settings.FirstOrDefault(ltis => ltis.Name == this.ListType);
-      }
-      else
-      {
-        settings = new List<ListTypeItemSettings>();
-      }
+		private void SaveSettings()
+		{
+			ListTypeItemSettings mySetting = null;
+			var settingsFile = new FileInfo(GetSettingsStorageFile());
 
-      if (mySetting == null)
-      {
-        mySetting = new ListTypeItemSettings();
-        mySetting.Name = this.ListType;
-        mySetting.GridSettingsFile = settingsFile.DirectoryName + "\\grid_settings_" + mySetting.Name + ".xml";
-        settings.Add(mySetting);
-      }
+			var settings = GetSettingsList();
+			if (settings != null)
+			{
+				mySetting = settings.FirstOrDefault(ltis => ltis.Name == this.ListType);
+			}
+			else
+			{
+				settings = new List<ListTypeItemSettings>();
+			}
 
-      mySetting.Location = this.Location;
+			if (mySetting == null)
+			{
+				mySetting = new ListTypeItemSettings();
+				mySetting.Name = this.ListType;
+				mySetting.GridSettingsFile = settingsFile.DirectoryName + "\\grid_settings_" + mySetting.Name + ".xml";
+				settings.Add(mySetting);
+			}
 
-      if (this.WindowState == FormWindowState.Normal)
-      {
-        mySetting.Size = this.Size;
-      }
-      else
-      {
-        mySetting.Size = this.RestoreBounds.Size;
-      }
-      string xml = SerializeToXml(settings);
-      File.WriteAllText(settingsFile.FullName, xml);
-      this.grid.SaveLayout(mySetting.GridSettingsFile);
-    }
+			mySetting.Location = this.Location;
 
-    private List<ListTypeItemSettings> GetSettingsList()
-    {
-      string settingsFile = GetSettingsStorageFile();
-      if (File.Exists(settingsFile))
-      {
-        return DeserializeFromXml<List<ListTypeItemSettings>>(settingsFile);
-      }
-      else
-      {
-        return null;
-      }
-    }
+			if (this.WindowState == FormWindowState.Normal)
+			{
+				mySetting.Size = this.Size;
+			}
+			else
+			{
+				mySetting.Size = this.RestoreBounds.Size;
+			}
+			string xml = SerializeToXml(settings);
+			File.WriteAllText(settingsFile.FullName, xml);
+			this.grid.SaveLayout(mySetting.GridSettingsFile);
+		}
 
-    private string GetSettingsStorageFile()
-    {
-      string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-      return Path.Combine(assemblyFolder, "ListDebuggerVisualizerSettings.xml");
-    }
+		private List<ListTypeItemSettings> GetSettingsList()
+		{
+			string settingsFile = GetSettingsStorageFile();
+			if (File.Exists(settingsFile))
+			{
+				return DeserializeFromXml<List<ListTypeItemSettings>>(settingsFile);
+			}
+			else
+			{
+				return null;
+			}
+		}
 
-    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
-    {
-      SaveSettings();
-    }
+		private string GetSettingsStorageFile()
+		{
+			string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			return Path.Combine(assemblyFolder, "ListDebuggerVisualizerSettings.xml");
+		}
 
-    public static string SerializeToXml(object obj)
-    {
-      var serializer = new XmlSerializer(obj.GetType());
-      StringWriter sw = new StringWriter();
-      serializer.Serialize(sw, obj);
-      return sw.ToString();
-    }
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (this.formLoaded)
+			{
+				SaveSettings();
+			}
+		}
 
-    public static T DeserializeFromXml<T>(string xmlFilePath)
-    {
-      var serializer = new XmlSerializer(typeof(T));
-      T obj = default(T);
-      using (TextReader textReader = new StreamReader(xmlFilePath))
-      {
-        obj = (T)serializer.Deserialize(textReader);
-      }
-      return obj;
-    }
+		public static string SerializeToXml(object obj)
+		{
+			var serializer = new XmlSerializer(obj.GetType());
+			StringWriter sw = new StringWriter();
+			serializer.Serialize(sw, obj);
+			return sw.ToString();
+		}
 
-    private void toolStripButtonExportToExcel_Click(object sender, EventArgs e)
-    {
-      var pack = new ExcelPackage();
-      var ws = pack.Workbook.Worksheets.Add(this.ListType);
+		public static T DeserializeFromXml<T>(string xmlFilePath)
+		{
+			var serializer = new XmlSerializer(typeof(T));
+			T obj = default(T);
+			using (TextReader textReader = new StreamReader(xmlFilePath))
+			{
+				obj = (T)serializer.Deserialize(textReader);
+			}
+			return obj;
+		}
 
-      int col = 1;
-      int row = 1;
-      var itemType = this.Model.GetType().GetProperty("Item").PropertyType;
-      var properties = itemType.GetProperties();
-      foreach (var prop in properties)
-      {
-        ws.Cells[row, col].Value = prop.Name;
-        col++;
-      }
+		private void toolStripButtonExportToExcel_Click(object sender, EventArgs e)
+		{
+			var pack = new ExcelPackage();
+			var ws = pack.Workbook.Worksheets.Add(this.ListType);
 
-      row++;
-      col = 1;
-      foreach (var item in this.Model)
-      {
-        foreach (var prop in properties)
-        {
-          var value = prop.GetValue(item, new object[0]);
-          if (value != null)
-            ws.Cells[row, col].Value = value.ToString();
-          col++;
-        }
-        row++;
-        col = 1;
-      }
+			int col = 1;
+			int row = 1;
+			var itemType = this.Model.GetType().GetProperty("Item").PropertyType;
+			var properties = itemType.GetProperties();
+			foreach (var prop in properties)
+			{
+				ws.Cells[row, col].Value = prop.Name;
+				col++;
+			}
 
-      var sd = new SaveFileDialog();
-      sd.FileName = this.ListType + ".xlsx";
-      sd.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
-      if (sd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-      {
-        using (FileStream fs = new FileStream(sd.FileName, FileMode.Create))
-        {
-          pack.SaveAs(fs);
-        }
-      }
-    }
+			row++;
+			col = 1;
+			foreach (var item in this.Model)
+			{
+				foreach (var prop in properties)
+				{
+					var value = prop.GetValue(item, new object[0]);
+					if (value != null)
+						ws.Cells[row, col].Value = value.ToString();
+					col++;
+				}
+				row++;
+				col = 1;
+			}
 
-    private void toolStripButtonClearTypeSettings_Click(object sender, EventArgs e)
-    {
-      this.grid.DataSource = null;
-      this.grid.DataSource = this.Model;
-      AutoFitColumns();
-      SaveSettings();
-    }
+			var sd = new SaveFileDialog();
+			sd.FileName = this.ListType + ".xlsx";
+			sd.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+			if (sd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				using (FileStream fs = new FileStream(sd.FileName, FileMode.Create))
+				{
+					pack.SaveAs(fs);
+				}
+			}
+		}
 
-  }
+		private void toolStripButtonClearTypeSettings_Click(object sender, EventArgs e)
+		{
+			this.grid.DataSource = null;
+			this.grid.DataSource = this.Model;
+			AutoFitColumns();
+			SaveSettings();
+		}
 
-  [Serializable]
-  public class ListTypeItemSettings
-  {
-    public string Name { get; set; }
-    public Point Location { get; set; }
-    public Size Size { get; set; }
-    public string GridSettingsFile { get; set; }
-  }
+	}
+
+	[Serializable]
+	public class ListTypeItemSettings
+	{
+		public string Name { get; set; }
+		public Point Location { get; set; }
+		public Size Size { get; set; }
+		public string GridSettingsFile { get; set; }
+	}
 }
